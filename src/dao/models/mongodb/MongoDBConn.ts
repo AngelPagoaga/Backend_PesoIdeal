9ce0a5872dffa279d5d7ea30505e3d24602fef10
Mario Jose <jomar200120@gmail.com>
2022-12-04 16:21:26 -0600

    Todo lisro en registro

--- src/dao/models/mongodb/MongoDBConn.ts
+++ src/dao/models/mongodb/MongoDBConn.ts
@@ -2,7 +2,7 @@ import { MongoClient } from 'mongodb';
 
 let connection:MongoClient = null;
 let mongoURI = process.env.MONGO_URI || 'mongodb://localhost:27017';
-let mongoDBName = process.env.MONGO_DB_NAME || 'sw202203';
+let mongoDBName = process.env.MONGO_DB_NAME || 'peso_ideal';
 
 export const getConnection = async ()=> {
   if( !connection){
